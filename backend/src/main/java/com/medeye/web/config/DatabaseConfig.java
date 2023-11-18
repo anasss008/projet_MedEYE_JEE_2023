5f2fa5afc551eab8112238b721a89badd66c8d53
yassineiscoding <yassine.elbadraoui@gmail.com>
2023-11-18 11:17:55 +0100

    Simplified DatabaseConfig singleton.

--- backend/src/main/java/com/medeye/web/config/DatabaseConfig.java
+++ backend/src/main/java/com/medeye/web/config/DatabaseConfig.java
@@ -1,49 +1,32 @@
 package com.medeye.web.config;
 
-import javax.sql.DataSource;
 
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.medeye.web.repositories")
-@EnableTransactionManagement
 public class DatabaseConfig {
+
 
     @Bean
     public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/Health");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("anas");
-        return dataSource;
+        return DataSourceBuilder.create()
+                .url("jdbc:postgresql://localhost:5432/Health")
+                .username("postgres")
+                .password("Pxhd4850")
+                .build();
     }
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource());
-        em.setPackagesToScan("com.medeye.web.entities");
-
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        em.setJpaVendorAdapter(vendorAdapter);
-
-        return em;
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 
-    /*@Bean
-    public PlatformTransactionManager transactionManager() {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
-        return transactionManager;
-    }*/
 
 }
--- backend/src/main/java/com/medeye/web/repositories/PatientRepository.java
+++ backend/src/main/java/com/medeye/web/repositories/PatientRepository.java
@@ -10,7 +10,5 @@ import java.util.Optional;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient,Long> {
-    Optional<Patient> findByUsername(String username);
-
 
 }
